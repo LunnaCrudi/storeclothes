9f9a21706373589e1adef8d335219aed24bd5a15
LunnaCrudi <lunnacrudi@gmail.com>
2022-05-31 21:49:10 -0300

    aula 19

---
+++ moon streetwar/js/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let cont = 0;
+do
+{
+    console.log("Número : " + cont);
+    cont = cont++; //ou cont++
+}while (cont <= 15);
---
+++ moon streetwar/js/testes/teste_2.js
@@ -0,0 +1,10 @@
+let limite = 100;
+let saldo_atual =20;
+
+let resultado = (saldo_atual < limite) ? "Necessito depositar $$" : "Não necessita depósito";
+
+console.log(resultado)
+/*
+Sintaxe:
+let variável = (condição) ? // instrução se true : // instrução se false
+*/
